0620b26e051d10321d3d661fc9f9678e2a89ada7
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-02-01 18:15:33 +0900

    start setup capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :application, "Erobot"
+set :repository,  "git@github.com:sanojimaru/Erobot.git"
+set :scm, :git
+set :user, "www"
+set :branch, "develop"
+set :deploy_to,  "/var/www/#{application}"
+set :use_sudo, false
+
+role :web, "capistrano_rails.gameraworks.com"
+role :app, "capistrano_rails.gameraworks.com"
+role :db,  "capistrano_rails.gameraworks.com", primary: true
+
+namespace :deploy do
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
---
+++ spider/spider.rb
@@ -0,0 +1,77 @@
+require 'net/http'
+require 'uri'
+require 'rss'
+require 'nokogiri'
+require 'charlock_holmes'
+
+ero_sites = {
+  #'http://www.pinkchannnel.com/' => {
+    #rss: 'http://www.pinkchannnel.com/index.rdf',
+    #title_xpath: '//div[@id="main"]//div[@class="article-header"]//h2',
+    #img_thumb_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]',
+    #img_link_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]/..',
+  #},
+  #'http://bokkisokuho.ldblog.jp/' => {
+    #rss: 'http://bokkisokuho.ldblog.jp/index.rdf',
+    #title_xpath: '//div[@id="main"]//div[@class="article-header"]//h2',
+    #img_thumb_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]',
+    #img_link_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]/..',
+  #},
+  #'http://minkch.com/' => {
+    #rss: 'http://minkch.com/index.rdf',
+    #title_xpath: '//div[@id="main"]//div[@class="article-header"]//h2',
+    #img_thumb_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]',
+    #img_link_xpath: '//div[@id="main"]//div[@class="article-body-inner"]//a/img[@class="pict"]/..',
+  #},
+  'http://okkisokuho.blog107.fc2.com/' => {
+    rss: 'http://okkisokuho.blog107.fc2.com/?xml',
+    title_xpath: '//*',
+    img_thumb_xpath: '//div[@class="main_cont"]/div[@class="entry_box"]/div[@class="entry_text"]/a/img',
+    img_link_xpath: '//div',
+  },
+  #'http://hitorihnoyoru.blog104.fc2.com/' => {
+    #rss: 'http://hitorihnoyoru.blog104.fc2.com/?xml',
+    #title_xpath: '//div[@class="main_cont"]/div[@class="entry_box"]/h2[@class="entry_title"]',
+    #img_thumb_xpath: '//div[@class="main_cont"]/div[@class="entry_box"]/div[@class="entry_text"]/a/img',
+    #img_link_xpath: '//div',
+  #},
+}
+
+ero_sites.each do |domain, site|
+  rss_body = Net::HTTP.get URI.parse(site[:rss])
+  rss = nil
+
+  begin
+    rss = RSS::Parser.parse(rss_body)
+  rescue RSS::InvalidRSSError
+    rss = RSS::Parser.parse(rss_body, false)
+  end
+  next if rss.nil?
+
+  site_title = rss.channel.title
+  puts site_title
+
+  rss.items.each do |item|
+    page_url = URI.parse(item.link)
+    puts page_url
+    page_body = Net::HTTP.get page_url
+    detection = CharlockHolmes::EncodingDetector.detect(page_body)
+    page_body = CharlockHolmes::Converter.convert page_body, detection[:encoding], 'UTF-8'
+
+    page = Nokogiri::XML::Document.parse(page_body, nil, detection[:encoding])
+    puts page.errors
+    break
+
+    page_title = page.xpath(site[:title_xpath]).text
+    puts page_title
+
+    page_image_elements = page.xpath(site[:img_link_xpath])
+    puts page_image_elements
+
+    page_images = page_image_elements
+      .select{|link| link.attr(:href).match /.*\.(jpg|jpeg|gif|png)$/ }
+      .map{|link| {url: link.attr(:href), text: link.attr(:title)}}
+    puts page_images
+    break
+  end
+end
