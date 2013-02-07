8e3ccdea756f9804430b7c0e990f7547b4e3b509
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-02-08 01:17:24 +0900

    add exception handler

--- app/spiders/spider.rb
+++ app/spiders/spider.rb
@@ -11,47 +11,47 @@ class Spider
     s3uploader = S3Upload.new
 
     Site.all.each do |site|
-      rss_body = open(site.rss).read
-      detection = CharlockHolmes::EncodingDetector.detect(rss_body)
-      rss = Nokogiri::XML rss_body.encode('UTF-8', detection[:encoding], undef: :replace, invalid: :replace), nil, 'UTF-8'
+      begin
+        rss_body = open(site.rss).read
+        detection = CharlockHolmes::EncodingDetector.detect(rss_body)
+        rss = Nokogiri::XML rss_body.encode('UTF-8', detection[:encoding], undef: :replace, invalid: :replace), nil, 'UTF-8'
+      rescue => e
+        puts e
+      end
 
       rss.css('item').each do |item|
-        page_url = URI.parse item.attr('rdf:about')
-        page_body = open(page_url).read
-        detection = CharlockHolmes::EncodingDetector.detect(page_body)
-        doc = Nokogiri::HTML page_body.encode('UTF-8', detection[:encoding], undef: :replace, invalid: :replace), nil, 'UTF-8'
-        page_title = doc.css(site.title_css).text
-
         begin
+          page_url = URI.parse item.attr('rdf:about')
+          page_body = open(page_url).read
+          detection = CharlockHolmes::EncodingDetector.detect(page_body)
+          doc = Nokogiri::HTML page_body.encode('UTF-8', detection[:encoding], undef: :replace, invalid: :replace), nil, 'UTF-8'
+          page_title = doc.css(site.title_css).text
+
           page = Page.find_or_create_by_url! page_url.to_s, site: site, title: page_title, url: page_url.to_s
         rescue => e
-          puts e.class
+          puts e
           next
         end
 
-        puts "*" * 20
-        puts "#{page_title} : #{page_url}"
-
         doc.css(site.img_css).each do |img|
-          link = img.parent
-          param = {
-            page: page,
-            content: link.attr(:title),
-            url: link.attr(:href),
-            thumb_url: img.attr(:src),
-          }
-
-          puts param
-
-          tmp_image_dir = Rails.root.join('tmp', 'images')
-          FileUtils.mkdir tmp_image_dir unless File.exists?(tmp_image_dir)
-
-          filename = downloader.get param[:url], tmp_image_dir
-          s3uploader.put filename, [site.id, page.id].join('/')
-          FileUtils.rm filename
-
-          next unless param[:url] =~ /\.(jpg|jpeg|gif|png)$/
           begin
+            link = img.parent
+            param = {
+              page: page,
+              content: link.attr(:title),
+              url: link.attr(:href),
+              thumb_url: img.attr(:src),
+            }
+
+            tmp_image_dir = Rails.root.join('tmp', 'images')
+            FileUtils.mkdir tmp_image_dir unless File.exists?(tmp_image_dir)
+
+            filename = downloader.get param[:url], tmp_image_dir
+            s3uploader.put filename, [site.id, page.id].join('/')
+            FileUtils.rm filename
+
+            next unless param[:url] =~ /\.(jpg|jpeg|gif|png)$/
+
             Image.create! page: page, content: link.attr(:title), url: link.attr(:href), thumb_url: img.attr(:src)
           rescue => e
             puts e
