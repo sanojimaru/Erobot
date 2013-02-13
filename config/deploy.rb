f300c5f3fc1839cfb143737f53a90a360d501520
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-02-14 01:29:42 +0900

    add deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+load "deploy/assets"
 require "bundler/capistrano"
 require "whenever/capistrano"
 
@@ -14,12 +15,39 @@ set :use_sudo, false
 set :whenever_roles, "app"
 set :whenever_command, "bundle exec whenever"
 
-role :web, "capistrano_rails.gameraworks.com"
-role :app, "capistrano_rails.gameraworks.com"
-role :db,  "capistrano_rails.gameraworks.com", primary: true
+set :domain, "capistrano_rails.gameraworks.com"
+
+role :web, domain
+role :app, domain
+role :db,  domain, primary: true
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  task :create_directory do
+    run <<-CMD
+      mkdir -p #{shared_path}/sockets
+    CMD
+  end
+
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
 end
+
+require "capistrano-unicorn"
+after 'deploy:restart', 'unicorn:restart'
+after 'deploy:create_symlink', 'deploy:create_directory'
